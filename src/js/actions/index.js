d7ca50f1bc23da479283b2fd7bc5379964a03d83
AceDZN <design@acedzn.com>
2017-12-14 01:33:22 +0200

    "apply-prod-environment"

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '806a11197dbdfbd7f81f86ffb1fbb565';
-const FORECAST_URL = `http://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${API_KEY}`;
+const FORECAST_URL = `https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${API_KEY}`;
 
 export const FETCH_FORECAST = 'FETCH_FORECAST';
 
